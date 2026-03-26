4fe9bb05a50570005d09c96f7dea118c2132b285
alexngumbau <alexngumbau48@gmail.com>
2026-03-26 10:55:57 +0300

    added animation to the banner, featured section, hero, navbar and newskater

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
 import { assets, cityList } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
+import { delay, motion } from "motion/react";
 
 const Hero = () => {
   const [pickupLocation, setPickLocation] = useState("");
-  const {pickupDate, setPickupDate, returnDate, setReturnDate, navigate} = useAppContext();
+  const { pickupDate, setPickupDate, returnDate, setReturnDate, navigate } =
+    useAppContext();
 
   const handleSearch = (e) => {
     e.preventDefault();
-    navigate('/cars?pickupLocation=' + pickupLocation + '&pickupDate=' + pickupDate + '&returnDate=' + returnDate);
-
-  }
+    navigate(
+      "/cars?pickupLocation=" +
+        pickupLocation +
+        "&pickupDate=" +
+        pickupDate +
+        "&returnDate=" +
+        returnDate,
+    );
+  };
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
-      <h1 className="text-4xl md:text-5xl font-semibold">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+      className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center"
+    >
+      <motion.h1
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        className="text-4xl md:text-5xl font-semibold"
+      >
         Luxury cars on Rent
-      </h1>
+      </motion.h1>
 
-      <form onSubmit={handleSearch} className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]">
+      <motion.form
+        initial={{ scale: 0.95, opacity: 0, y: 50 }}
+        animate={{ scale: 1, opacity: 1, y:0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+        onSubmit={handleSearch}
+        className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
+      >
         <div className="flex flex-col md:flex-row items-start md:items-center gap-10 md:ml-8">
           <div className="flex flex-col items-start gap-2">
             <select
@@ -43,7 +67,7 @@ const Hero = () => {
               type="date"
               id="pickup-date"
               value={pickupDate}
-              onChange={e => setPickupDate(e.target.value)}
+              onChange={(e) => setPickupDate(e.target.value)}
               min={new Date().toISOString().split("T")[0]}
               className="text-sm text-gray-500"
               required
@@ -56,25 +80,32 @@ const Hero = () => {
               type="date"
               id="return-date"
               value={returnDate}
-              onChange={e => setReturnDate(e.target.value)}
+              onChange={(e) => setReturnDate(e.target.value)}
               className="text-sm text-gray-500"
               required
             />
           </div>
         </div>
 
-        <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer">
+        <motion.button
+          whileHover={{scale: 1.05}}
+          whileTap={{scale: 0.95}}
+         className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer">
           <img
             src={assets.search_icon}
             alt="search"
             className="brightness-300"
           />
           Search
-        </button>
-      </form>
+        </motion.button>
+      </motion.form>
 
-      <img src={assets.main_car} alt="car" className="max-h-74" />
-    </div>
+      <motion.img 
+      initial={{y: 100, opacity: 0}}
+      animate={{y: 0, opacity: 1}}
+      transition={{duration: 0.8, delay: 0.6}}
+      src={assets.main_car} alt="car" className="max-h-74" />
+    </motion.div>
   );
 };
 
